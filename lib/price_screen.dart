b9d5f86acb1d842ce2d5d86fbdcc5e7e2a5631fa
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-18 18:50:05 +0530

    add some comments to the code

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -11,6 +11,7 @@ class _PriceScreenState extends State<PriceScreen> {
 
   List<DropdownMenuItem> getDropdownCurrenciesList() {
     List<DropdownMenuItem<String>> dropdownItems = [];
+    // A simple for loop can also be used but for-in (for-each) loop is more appropriate to use.
     for (String currency in currenciesList) {
       var newItem = DropdownMenuItem(
         child: Text(currency),
