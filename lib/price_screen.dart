a45ca5db338277a2d4321ad449b266032ad445fa
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-18 14:33:48 +0530

    provide the type for the items of the DropDownButton() widget

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -42,7 +42,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton(),
+            child: DropdownButton<String>(),
           ),
         ],
       ),
