585eb791f8365dd86548afe7f9e36cce0b3bb308
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-18 18:47:12 +0530

    Comment out some part of the code

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -60,18 +60,18 @@ class _PriceScreenState extends State<PriceScreen> {
             child: DropdownButton<String>(
               value: selectedCurrency,
               items: [
-                DropdownMenuItem(
-                  child: Text('USD'),
-                  value: 'USD',
-                ),
-                DropdownMenuItem(
-                  child: Text('EUR'),
-                  value: 'EUR',
-                ),
-                DropdownMenuItem(
-                  child: Text('INR'),
-                  value: 'INR',
-                ),
+                // DropdownMenuItem(
+                //   child: Text('USD'),
+                //   value: 'USD',
+                // ),
+                // DropdownMenuItem(
+                //   child: Text('EUR'),
+                //   value: 'EUR',
+                // ),
+                // DropdownMenuItem(
+                //   child: Text('INR'),
+                //   value: 'INR',
+                // ),
               ],
               onChanged: (value) {
                 setState(() {
