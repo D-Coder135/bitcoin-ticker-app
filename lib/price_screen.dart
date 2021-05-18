a7d6a4d280628b4c54116e66921cf4268b621a85
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-18 22:24:20 +0530

    pass the field selectedCurrency inside the Text() widget which is the child of the Padding() widget

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -79,7 +79,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = ? $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
