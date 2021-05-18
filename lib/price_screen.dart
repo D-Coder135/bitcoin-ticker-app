dc2d3bf0eebedecaf6851dc63c6bbecbc0fd459b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-18 19:59:05 +0530

    add the ternary operator along with the appropriate methods call inside the child property of the Container() widget

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -94,7 +94,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: getPicker(),
+            child: Platform.isIOS ? getiOSPicker() : getAndroidDropdown(),
           ),
         ],
       ),
