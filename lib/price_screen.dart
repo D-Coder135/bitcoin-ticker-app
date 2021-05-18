1246a2d8c19abdd5f8c50ec07de2b46b720d520a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-18 19:34:28 +0530

    Uncomment out some part of the code

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -10,7 +10,32 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
 
-  DropdownButton<String> getDropdownButton() {}
+  DropdownButton<String> getDropdownButton() {
+    DropdownButton<String>(
+      value: selectedCurrency,
+      items: getDropdownCurrenciesList(),
+// items: [
+//   DropdownMenuItem(
+//     child: Text('USD'),
+//     value: 'USD',
+//   ),
+//   DropdownMenuItem(
+//     child: Text('EUR'),
+//     value: 'EUR',
+//   ),
+//   DropdownMenuItem(
+//     child: Text('INR'),
+//     value: 'INR',
+//   ),
+// ],
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+        });
+      },
+    )
+    ,
+  }
 
   List<DropdownMenuItem> getDropdownCurrenciesList() {
     List<DropdownMenuItem<String>> dropdownItems = [];
