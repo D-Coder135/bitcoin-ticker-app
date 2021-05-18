29ed825288a756d755cfc6b3e06bed692d5a79c0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-18 19:33:44 +0530

    cut out some comment from the code

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -84,27 +84,3 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 }
-
-// DropdownButton<String>(
-// value: selectedCurrency,
-// items: getDropdownCurrenciesList(),
-// // items: [
-// //   DropdownMenuItem(
-// //     child: Text('USD'),
-// //     value: 'USD',
-// //   ),
-// //   DropdownMenuItem(
-// //     child: Text('EUR'),
-// //     value: 'EUR',
-// //   ),
-// //   DropdownMenuItem(
-// //     child: Text('INR'),
-// //     value: 'INR',
-// //   ),
-// // ],
-// onChanged: (value) {
-// setState(() {
-// selectedCurrency = value;
-// });
-// },
-// ),
