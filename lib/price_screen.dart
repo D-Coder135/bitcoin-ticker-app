247e0e409391b71bcf37a72a1eb5de7df9854da0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-23 19:09:07 +0530

    Comment out some part of the code

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -93,69 +93,69 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $currentRateForBTC $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 ETH = $currentRateForETH $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 LTC = $currentRateForLTC $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          // Padding(
+          //   padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          //   child: Card(
+          //     color: Colors.lightBlueAccent,
+          //     elevation: 5.0,
+          //     shape: RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(10.0),
+          //     ),
+          //     child: Padding(
+          //       padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          //       child: Text(
+          //         '1 BTC = $currentRateForBTC $selectedCurrency',
+          //         textAlign: TextAlign.center,
+          //         style: TextStyle(
+          //           fontSize: 20.0,
+          //           color: Colors.white,
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // Padding(
+          //   padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          //   child: Card(
+          //     color: Colors.lightBlueAccent,
+          //     elevation: 5.0,
+          //     shape: RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(10.0),
+          //     ),
+          //     child: Padding(
+          //       padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          //       child: Text(
+          //         '1 ETH = $currentRateForETH $selectedCurrency',
+          //         textAlign: TextAlign.center,
+          //         style: TextStyle(
+          //           fontSize: 20.0,
+          //           color: Colors.white,
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // Padding(
+          //   padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          //   child: Card(
+          //     color: Colors.lightBlueAccent,
+          //     elevation: 5.0,
+          //     shape: RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(10.0),
+          //     ),
+          //     child: Padding(
+          //       padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          //       child: Text(
+          //         '1 LTC = $currentRateForLTC $selectedCurrency',
+          //         textAlign: TextAlign.center,
+          //         style: TextStyle(
+          //           fontSize: 20.0,
+          //           color: Colors.white,
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
