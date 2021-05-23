23a7507b3aab432efdd42a33990551242b401f36
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-23 19:05:30 +0530

    pass the field currentRateForLTC to the Text() widget

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -146,7 +146,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 LTC = $currentRateForBTC $selectedCurrency',
+                  '1 LTC = $currentRateForLTC $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
